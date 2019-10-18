4be8b127ab12f6512cf9b605e1b745d69e25c549
Roddymal <RodwinMalinga@Gmail.com>
2019-10-18 12:47:40 +0200

    Added Em

---
+++ Game/ClarencePlatformer2/Assets/ClarencePlatformerAssets/Scripts/DeathTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneChange.spawnPoint = new Vector3(spawnPoint.position.x, spawnPoint.position.y, other.transform.position.z);
+            other.gameObject.GetComponent<ClarenceMovement>().LoseLife();
+        }
+    }
+}
---
+++ Game/ClarencePlatformer2/Assets/ClarencePlatformerAssets/Scripts/WalkSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkSensor : MonoBehaviour
+{
+    public bool beenEntered = false;     // true after player leaves its collider
+    public bool fullyActive = false;     // true when flame lerped to full size
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            beenEntered = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+
+    }
+}
