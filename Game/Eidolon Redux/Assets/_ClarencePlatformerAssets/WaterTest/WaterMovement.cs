bf5963a8e561d3f08cc3f22d66da5460fd6e97e4
Erik Prinz <erik88d3@gmail.com>
2019-10-30 09:15:24 +0200

    Fixed water movement

--- Game/Eidolon Redux/Assets/_ClarencePlatformerAssets/WaterTest/WaterMovement.cs
+++ Game/Eidolon Redux/Assets/_ClarencePlatformerAssets/WaterTest/WaterMovement.cs
@@ -9,20 +9,16 @@ public class WaterMovement : MonoBehaviour
     {
         public Renderer rend;
         public float speed;
+        public float lerpSpeed;
         public bool randSpeed;
         public float randSpeedMaxChange;
+        [HideInInspector] public float currentXTarget;
     }
 
     public RendererToMove[] rends;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         for (int i = 0; i < rends.Length; i++)
         {
@@ -32,7 +28,36 @@ public class WaterMovement : MonoBehaviour
             {
                 currentSpeed += Random.Range(-rends[i].randSpeedMaxChange, rends[i].randSpeedMaxChange);
             }
-            rends[i].rend.material.SetTextureOffset("_MainTex", new Vector2((currentXOffset + (currentSpeed * Time.deltaTime)) % 1, 0));
+            rends[i].currentXTarget = (currentXOffset + (currentSpeed * Time.deltaTime));
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < rends.Length; i++)
+        {
+            float currentXOffset = rends[i].rend.material.GetTextureOffset("_MainTex").x;      //the current x offset of our texture
+
+            if (Mathf.Abs(currentXOffset - rends[i].currentXTarget) < 0.4f)  // checks if the lerp is done
+            {
+                float currentSpeed = rends[i].speed;
+                if (rends[i].randSpeed)
+                {
+                    currentSpeed += Random.Range(-rends[i].randSpeedMaxChange, rends[i].randSpeedMaxChange);
+                }
+
+                rends[i].currentXTarget = (currentXOffset + (currentSpeed * Time.deltaTime));
+
+                Vector2 target = Vector2.Lerp(new Vector2(currentXOffset, 0), new Vector2(rends[i].currentXTarget, 0), rends[i].lerpSpeed * Time.deltaTime);
+                rends[i].rend.material.SetTextureOffset("_MainTex", target);
+            }
+            else
+            {
+                Vector2 target = Vector2.Lerp(new Vector2(currentXOffset, 0), new Vector2(rends[i].currentXTarget, 0), rends[i].lerpSpeed * Time.deltaTime);
+                rends[i].rend.material.SetTextureOffset("_MainTex",target);
+            }
         }
     }
 }
