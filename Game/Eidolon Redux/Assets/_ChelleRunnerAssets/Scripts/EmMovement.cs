9ec408077dbae0f8e43567643902e636b04df75e
Erik Prinz <erik88d3@gmail.com>
2019-11-03 14:11:22 +0200

    EmRunnerController fixed and smoothed

--- Game/Eidolon Redux/Assets/_ChelleRunnerAssets/Scripts/EmMovement.cs
+++ Game/Eidolon Redux/Assets/_ChelleRunnerAssets/Scripts/EmMovement.cs
@@ -14,64 +14,275 @@ public class EmMovement : MonoBehaviour
 
     public Lane currentLane = Lane.Middle;
 
+    // the following are used to track Em's current movement parameters
+    // Note: Em can turn while jumping and jump while turning - but this doesn't apply to sliding
+    // Note: Em can slide out of jumping and jump out of sliding
+    public enum EmSpeedState
+    {
+        Running,
+        Jogging,
+        Walking,
+        Stationary
+    }
+
+    public enum EmSpecialState
+    {
+        Sliding,
+        Jumping,
+        None
+    }
+
+    public EmSpecialState specialState = EmSpecialState.None;
+    public EmSpeedState speedState = EmSpeedState.Stationary;
+    public bool emTurning = false;
+
     public Transform[] laneMarkers;   // marks the centre for each lane
 
     Transform currentTarget;
 
     public float laneShiftSpeed = 3;
+    public float jumpSpeed = 3;
+    public float slideFallSpeed = 3;
+    public float slideDuration = 1f;
 
     public Animator emAnim;
     public SpriteRenderer spriteRend;
 
+    public BoxCollider slideBox;
+    public BoxCollider standardBox;
+
+    public float extraGravity = 1f;
+
+    Rigidbody emRigidBody;
+
+    //public Transform emSpriteTrans;
+
+    //public Transform jumpPoint;
+    //public Transform slidePoint;
+    Vector3 initPoint;
+
+    //bool jumpRising = false;
+    float slidingTimer = 0;
+
+    public LayerMask groundRayLayers;
+    public float groundRayStandardLength;
+    public float groundRaySlideLength;
+
+    float currentGroundRayLength = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         currentTarget = laneMarkers[(int)currentLane];
+        initPoint = transform.position;
+
+        emRigidBody = GetComponent<Rigidbody>();
+
+        emAnim.SetBool("walking", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement = Input.GetAxisRaw("Horizontal");
-
-        if (movement < 0)    // left move
+        if (speedState != EmSpeedState.Stationary)
         {
-            if (currentLane != Lane.Left)
+            float movement = Input.GetAxisRaw("Horizontal");
+
+            if (specialState != EmSpecialState.Sliding) // check if turn is allowed
             {
-                if (Mathf.Abs(transform.position.x - currentTarget.position.x) < 0.05f)
+                if (movement < 0)    // left move
                 {
-                    currentLane--;
-                    currentTarget = laneMarkers[(int)currentLane];
-                    emAnim.SetBool("switchLane", true);
+                    if (currentLane != Lane.Left)
+                    {
+                        if (Mathf.Abs(transform.position.x - currentTarget.position.x) < 0.05f)
+                        {
+                            currentLane--;
+                            currentTarget = laneMarkers[(int)currentLane];
+                            if (!emTurning)
+                            {
+                                emAnim.SetBool("switchLane", true);
+                            }
+                            spriteRend.flipX = false;
+                            emTurning = true;
+                        }
+                    }
+                }
+                else if (movement > 0) // right move
+                {
+                    if (currentLane != Lane.Right)
+                    {
+                        if (Mathf.Abs(transform.position.x - currentTarget.position.x) < 0.05f)
+                        {
+                            currentLane++;
+                            currentTarget = laneMarkers[(int)currentLane];
+                            if (!emTurning)
+                            {
+                                emAnim.SetBool("switchLane", true);
+                            }
+                            spriteRend.flipX = true;
+                            emTurning = true;
+                        }
+                    }
+                }
+            }
+
+            // turn control
+            if (emTurning)
+            {
+                if (Mathf.Abs(transform.position.x - currentTarget.position.x) > 0.05f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentTarget.position.x, transform.position.y, transform.position.z), laneShiftSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    emAnim.SetBool("switchLane", false);
                     spriteRend.flipX = false;
+                    emTurning = false;
+                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentTarget.position.x, transform.position.y, transform.position.z), laneShiftSpeed * Time.deltaTime);
                 }
             }
-        }
-        else if(movement > 0) // right move
-        {
-            if (currentLane != Lane.Right)
+
+            // jump control    //add raycast
+            if (specialState != EmSpecialState.Jumping && (Input.GetAxisRaw("Vertical") > 0.2f || Input.GetButtonDown("EmRunnerJump")))
             {
-                if (Mathf.Abs(transform.position.x - currentTarget.position.x) < 0.05f)
+                if (specialState == EmSpecialState.Sliding)
                 {
-                    currentLane++;
-                    currentTarget = laneMarkers[(int)currentLane];
-                    emAnim.SetBool("switchLane", true);
-                    spriteRend.flipX = true;
+                    currentGroundRayLength = groundRaySlideLength;
+                }
+                else
+                {
+                    currentGroundRayLength = groundRayStandardLength;
+                }
+
+                RaycastHit hit;
+                // Does the ray intersect with ground
+                if (Physics.Raycast(transform.position, -transform.up, out hit, currentGroundRayLength, groundRayLayers))
+                {
+                    emAnim.SetBool("switchLane", false);
+                    emAnim.SetBool("jumping", true);
+                    emAnim.SetBool("sliding", false);
+
+                    standardBox.enabled = true;
+                    slideBox.enabled = false;
+                    //jumpBox.enabled = true;
+                    // jumpRising = true;
+                    specialState = EmSpecialState.Jumping;
+
+                    emRigidBody.velocity = new Vector3(0, jumpSpeed, 0);
+
+                    slidingTimer = Time.time - slideDuration;   // makes sure slide is deactivated in future
                 }
             }
-        }
 
-        if (Mathf.Abs(transform.position.x - currentTarget.position.x) > 0.05f)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, laneShiftSpeed * Time.deltaTime);
-        }
-        else
-        {
-            emAnim.SetBool("switchLane", false);
-            spriteRend.flipX = false;
-        }
+            if (emAnim.GetBool("falling") == true)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, -transform.up, out hit, currentGroundRayLength, groundRayLayers))
+                {
+                    emAnim.SetBool("falling", false);
+                }
+            }
 
+            if (specialState == EmSpecialState.Jumping)
+            {
+                emRigidBody.AddForce(new Vector3(0, extraGravity, 0));       // use to improve feel
 
+                if (emRigidBody.velocity.y <= 0)
+                {
+                    emAnim.SetBool("switchLane", false);
+                    emAnim.SetBool("jumping", false);
+                    emAnim.SetBool("falling", true);
+                    
+                    RaycastHit hit;
+                    // Does the ray intersect with ground
+                    if (Physics.Raycast(transform.position, -transform.up, out hit, currentGroundRayLength, groundRayLayers))
+                    {
+                        specialState = EmSpecialState.None;
+                        print(hit.collider.gameObject.name);
+
+                        emAnim.SetBool("falling", false);
+                    }
+                }
+            }
+
+            // slide control
+            if ((!emTurning || specialState == EmSpecialState.Jumping) && specialState != EmSpecialState.Sliding && (Input.GetAxisRaw("Vertical") < -0.2f || Input.GetButtonDown("EmRunnerSlide")))
+            {
+                //emAnim.SetBool("switchLane", false);
+                emAnim.SetBool("jumping", false);
+                emAnim.SetBool("sliding", true);
+                emAnim.SetBool("switchLane", false);
+
+                emRigidBody.velocity = new Vector3(0, slideFallSpeed, 0);
+
+                //here we change the collider to the short one
+                standardBox.enabled = false;
+                slideBox.enabled = true;
+                //jumpBox.enabled = false;
+
+                slidingTimer = Time.time;
+                
+                specialState = EmSpecialState.Sliding;
+            }
+
+            if (specialState == EmSpecialState.Sliding)
+            {
+                //emSpriteTrans.position = Vector3.Lerp(emSpriteTrans.position, new Vector3(emSpriteTrans.position.x, initPoint.y, emSpriteTrans.position.z), 3 * Time.deltaTime);
+
+                RaycastHit hit;
+                // Does the ray intersect with ground
+                if (Physics.Raycast(transform.position, -transform.up, out hit, currentGroundRayLength, groundRayLayers))
+                {
+                    if (Time.time >= (slidingTimer + slideDuration))
+                    {
+                        specialState = EmSpecialState.None;
+
+                        standardBox.enabled = true;
+                        slideBox.enabled = false;
+
+                        emAnim.SetBool("sliding", false);
+                    }
+                }
+                else
+                {
+                    if (Time.time >= (slidingTimer + slideDuration))
+                    {
+                        slidingTimer = Time.time;
+                    }
+                }
+            }
+
+            Debug.DrawRay(transform.position, -transform.up * groundRaySlideLength, Color.yellow);
+            Debug.DrawRay(transform.position, -transform.up * groundRayStandardLength, Color.yellow);
+
+            
+
+        }
+    }
+
+    public void StartWalk()
+    {
+        emAnim.SetBool("walking", true);
+        emAnim.SetBool("running", false);
+        emAnim.SetBool("jogging", false);
+        speedState = EmSpeedState.Walking;
+    }
+
+    public void StartJog()
+    {
+        emAnim.SetBool("walking", false);
+        emAnim.SetBool("running", false);
+        emAnim.SetBool("jogging", true);
+
+        speedState = EmSpeedState.Jogging;
+    }
+
+    public void StartRun()
+    {
+        emAnim.SetBool("walking", false);
+        emAnim.SetBool("running", true);
+        emAnim.SetBool("jogging", false);
+
+        speedState = EmSpeedState.Running;
     }
 }
--- Game/Eidolon Redux/Assets/_ChelleRunnerAssets/Scripts/MaterialTreadmill.cs
+++ Game/Eidolon Redux/Assets/_ChelleRunnerAssets/Scripts/MaterialTreadmill.cs
@@ -6,9 +6,22 @@ public class MaterialTreadmill : MonoBehaviour
 {
     public Material treadMat;
 
-    public float xSpeed = 0;
-    public float ySpeed = 0;
+    //public float xSpeed = 0;
+    public float ySpeed = 0;                 // current y speed
 
+    //public float maxXSpeed = 5;
+    public float maxYSpeed = 5;              // maximum speed of Em
+
+    //public float xRampSpeed = 1;
+    public float yRampSpeed = 1;             
+
+    public float walkThreshhold = 0.1f;      // Em begins walking after this
+    public float jogThreshhold = 2;          // Em begins jogging after this
+    public float runThreshhold = 4;          // Em begins running after this
+
+    public EmMovement emMovement;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +31,37 @@ public class MaterialTreadmill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        treadMat.mainTextureOffset = new Vector2(treadMat.mainTextureOffset.x + (xSpeed * Time.deltaTime), treadMat.mainTextureOffset.y + (ySpeed * Time.deltaTime));
+        // increase speed if it's still smaller than the max speed.
+        if (Mathf.Abs(ySpeed) < Mathf.Abs(maxYSpeed))
+        {
+            ySpeed += yRampSpeed * Time.deltaTime;
+        }
+
+        // change anims based on speed
+        if (Mathf.Abs(ySpeed) >= runThreshhold)
+        {
+            emMovement.StartRun();
+        }
+        else if (Mathf.Abs(ySpeed) >= jogThreshhold)
+        {
+            emMovement.StartJog();
+        }
+        else if (Mathf.Abs(ySpeed) >= walkThreshhold)
+        {
+            emMovement.StartWalk();
+        }
+
+
+        treadMat.mainTextureOffset = new Vector2(treadMat.mainTextureOffset.x /*+ (xSpeed * Time.deltaTime)*/, treadMat.mainTextureOffset.y + (ySpeed * Time.deltaTime));
+    }
+
+    public void EmHalfCrash()
+    {
+
+    }
+
+    public void EmFullCrash()
+    {
+
     }
 }
