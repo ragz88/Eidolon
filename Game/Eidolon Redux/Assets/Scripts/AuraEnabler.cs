b8864c0077ba5186dd559a634ced4b3a91c85b24
Erik Prinz <erik88d3@gmail.com>
2019-11-10 10:59:38 +0200

    Fixed build bugs. Organised woods scene.

---
+++ Game/Eidolon Redux/Assets/Scripts/AuraEnabler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Aura2API;
+
+public class AuraEnabler : MonoBehaviour
+{
+    public AuraLight[] auraLights;
+    public AuraCamera[] auraCams;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < auraLights.Length; i++)
+        {
+            auraLights[i].enabled = true;
+        }
+
+        for (int i = 0; i < auraCams.Length; i++)
+        {
+            auraCams[i].enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
