475e1e305c6a4ad85bc7612cbc7f2f80504493eb
Erik Prinz <erik88d3@gmail.com>
2019-11-14 06:31:52 +0200

    Did gang AR. 2 pages left.

---
+++ Comic/Eidolon - Augment Reality/Assets/Scripts/ScalerSpriteActivator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalerSpriteActivator : MonoBehaviour
+{
+    public enum CompareMode
+    {
+        GreaterThan,
+        SmallerThan
+    }
+
+    public CompareMode compMode = CompareMode.GreaterThan;
+
+    public Vector3Ranger ranger;
+
+    public SpriteRenderer rend;
+
+    public float threshhold = 0.5f;
+
+    public float fadeSpeed = 1f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (compMode == CompareMode.GreaterThan)
+        {
+            if (ranger.currentPercent > threshhold && rend.color.a < 1)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, rend.color.a + (fadeSpeed * Time.deltaTime));
+            }
+            else if (rend.color.a > 0)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, rend.color.a - (fadeSpeed*Time.deltaTime));
+            }
+        }
+        else
+        {
+            if (ranger.currentPercent < threshhold && rend.color.a < 1)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, rend.color.a + (fadeSpeed * Time.deltaTime));
+            }
+            else if (rend.color.a > 0)
+            {
+                rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, rend.color.a - (fadeSpeed * Time.deltaTime));
+            }
+        }
+    }
+}
---
+++ Game/Eidolon Redux/Assets/_DDR_Assets/Scripts/LongFallingSpriteArrow.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongFallingSpriteArrow : MonoBehaviour
+{
+    public float fallSpeed = 1;
+    public float destroyHeight = -6f;                   // at this y position, arrow is automatically destroyed (if missed)
+
+    
+
+    public bool inSensor = false;                       // whether the arrow's in a sensor or not
+    public float relativeDistance = 0;                  // distance between sensor and bottom arrow
+    public bool isCurrentlyPressed = false;             // whether the arrow's button is pressed or not
+
+    public bool arrowAligned = false;                   // true when the bottom arrow is perfectly in the sensor
+
+
+    public Transform topArrow;
+    public Transform bottomArrow;
+
+    public SpriteRenderer longRend;
+
+    bool previousFrameHit = false;
+
+    public bool pressedBefore = false;
+
+    public bool shattered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -fallSpeed * Time.deltaTime, 0);       //move the arrow downward;
+
+        if (!shattered)
+        {
+            if (inSensor)
+            {
+                if (arrowAligned)
+                {
+                    bottomArrow.Translate(0, fallSpeed * Time.deltaTime, 0);       //keep bottom arrow stationary;
+                }
+
+                if (isCurrentlyPressed)
+                {
+                    //longRend.size = new Vector2(longRend.size.x, transform.position.y - bottomArrow.position.y);
+
+                    pressedBefore = true;
+                    previousFrameHit = true;
+                }
+                else
+                {
+                    if (previousFrameHit)
+                    {
+                        //longRend.size = new Vector2(longRend.size.x, transform.position.y - bottomArrow.position.y);
+
+                        previousFrameHit = false;
+                    }
+                    else if (pressedBefore)
+                    {
+                        shattered = true;
+                        // cause shatter
+                    }
+                }
+            }
+        }
+        else
+        {
+            print("Shattered");
+        }
+
+        if (topArrow.transform.position.y < destroyHeight)
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void successfulScore()
+    {
+        print("Damn Son!");
+    }
+
+    public void midScore()
+    {
+        print("midscore");
+        //Destroy(gameObject);
+    }
+
+    public void missScore()
+    {
+        print("no way man");
+        //Destroy(gameObject);
+    }
+}
