18f04fa43ac66c5d4f4a4505358decaf595f188b
Erik Prinz <erik88d3@gmail.com>
2019-11-22 08:06:14 +0200

    Added Sounds.

---
+++ Game/Eidolon Redux/Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    int currentClip = 0;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (source.isPlaying == false)
+        {
+            currentClip++;
+            source.clip = clips[currentClip];
+            source.Play();
+        }
+    }
+}
---
+++ Game/Eidolon Redux/Assets/Scripts/RiverSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverSound : MonoBehaviour
+{
+
+    public Transform playerTrans;
+    public Transform audioTrans;
+    public Transform[] riverPoints;
+
+    public float audioSpeed = 1f;
+
+    int currentRiverPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < riverPoints.Length; i++)
+        {
+            if (Vector3.Distance(playerTrans.position, riverPoints[currentRiverPoint].position) >
+                Vector3.Distance(playerTrans.position, riverPoints[i].position))
+            {
+                currentRiverPoint = i;
+            }
+        }
+
+        audioTrans.position = Vector3.MoveTowards(audioTrans.position, riverPoints[currentRiverPoint].position, audioSpeed * Time.deltaTime);
+    }
+}
